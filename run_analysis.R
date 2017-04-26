ab59d332ea9dbe9f9c802dadbf60487598efd9c2
pitt3800 <pitt3800@gmail.com>
2017-04-26 22:54:21 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(data.table)
 
 
-# 1 Open data
+# Open data
 
 featuresTest <- read.table("/Users/YJL/Google 드라이브/My R/UCI HAR Dataset/test/x_test.txt")
 activityTest <- read.table("/Users/YJL/Google 드라이브/My R/UCI HAR Dataset/test/y_test.txt")
@@ -20,7 +20,7 @@ activityLabels <- read.table("/Users/YJL/Google 드라이브/My R/UCI HAR Datase
 
 
 
-# 2 Merge the training and the test sets to create one data set
+# 1 Merge the training and the test sets to create one data set
 
 subject <- rbind(subjectTrain, subjectTest)
 activity <- rbind(activityTrain, activityTest)
@@ -33,7 +33,7 @@ colnames(subject) <- "Subject"
 completeData <- cbind(features,activity,subject)
 
 
-# 3 Extracts only the measurements on the mean and standard deviation for each measurement
+# 2 Extracts only the measurements on the mean and standard deviation for each measurement
 
 columnsWithMeanSTD <- grep(".*Mean.*|.*Std.*", names(completeData), ignore.case=TRUE)
 
@@ -43,7 +43,7 @@ dim(completeData)
 extractedData <- completeData[,requiredColumns]
 dim(extractedData)
 
-# 4 Uses descriptive activity names to name the activities in the data set
+# 3 Uses descriptive activity names to name the activities in the data set
 
 extractedData$Activity <- as.character(extractedData$Activity)
 for (i in 1:6){
@@ -52,7 +52,7 @@ extractedData$Activity[extractedData$Activity == i] <- as.character(activityLabe
 
 extractedData$Activity <- as.factor(extractedData$Activity
 
-# 5 Appropriately labels the data set with descriptive variable names
+# 4 Appropriately labels the data set with descriptive variable names
 
 names(extractedData)
 
@@ -73,7 +73,7 @@ names(extractedData)<-gsub("gravity", "Gravity", names(extractedData))
 
 names(extractedData)
 
-# Creates a second, independent tidy data set with the average of each variable for each activity and each subject
+# 5 Creates a second, independent tidy data set with the average of each variable for each activity and each subject
                                     
 extractedData$Subject <- as.factor(extractedData$Subject)
 extractedData <- data.table(extractedData)
